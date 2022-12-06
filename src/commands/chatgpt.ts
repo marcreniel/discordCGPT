b8055b07d5b3ecfb3d5413afb396f13f8c74872e
imedra <marcbernardino2005@gmail.com>
2022-12-06 17:13:50 -0600

    added comments and stabilized cgpt function

--- src/commands/chatgpt.ts
+++ src/commands/chatgpt.ts
@@ -1,9 +1,11 @@
+// Import Required Libraries
 import "dotenv/config"
 import { CommandInteraction, ModalSubmitInteraction, ThreadChannel } from "discord.js";
 import { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
 import { Discord, Slash, ModalComponent } from "discordx";
 import { ChatGPTAPI } from 'chatgpt';
 
+// Connecting to ChatGPT API (Unofficial) 
 const api = new ChatGPTAPI({ sessionToken: process.env.openai_token! })
 await api.ensureAuth()
 
@@ -16,7 +18,7 @@ export class discordcgpt {
       .setTitle("Discord ChatGPT")
       .setCustomId("discordcgpt");
 
-    // Create text input fields
+    // Create chat input fields
     const chatInputComponent = new TextInputBuilder()
       .setCustomId("chatInput")
       .setLabel("Input your prompt for ChatGPT to respond to:")
@@ -35,24 +37,30 @@ export class discordcgpt {
 
   
   @ModalComponent()
+  // Maps chatInput to const chatInput: string
   async discordcgpt(interaction: ModalSubmitInteraction): Promise<void> {
     const [chatInput] = ["chatInput"].map((id) =>
       interaction.fields.getTextInputValue(id)
     );
 
-    let response = await api.sendMessage(chatInput)
-
-    if (response.length >= 2000) {
-      response = response.substring(0, 1999);
+    // Attempts to send chatInput to ChatGPT api; If it returns a value it is shown to the user, else an error occurs
+    try {
+      await interaction.reply(
+        `ChatGPT is processing your prompt...`
+      ) 
+      let response = await api.sendMessage(chatInput);
+      if (response.length >= 2000) {
+        response = response.substring(0, 1999);
+      }
+      await interaction.editReply(
+        `${response}`
+      )    
+    } catch(err){
+      await interaction.reply(
+        "ChatGPT has timed out. Please try again."
+      )        
     }
 
-    await interaction.reply(
-      `ChatGPT is generating a response...`
-    )
-
-    await interaction.editReply(response);
-
     return;
-    
-  }
+  }  
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Import Required Libraries
 import "dotenv/config"
 import { Client } from "discordx";
 import { IntentsBitField } from "discord.js";
@@ -5,6 +6,7 @@ import { dirname, importx } from "@discordx/importer";
 
 async function start() {
     const client = new Client({
+        // Declare Intents
         intents: [
             IntentsBitField.Flags.Guilds,
             IntentsBitField.Flags.GuildMessages,
@@ -14,21 +16,24 @@ async function start() {
     });
   
     client.once("ready", async () => {
+        // Flushes global application command cache and reinitializes them
         await client.clearApplicationCommands();
         await client.initApplicationCommands();
     });
-  
+    
     client.on("interactionCreate", (interaction) => {
         client.executeInteraction(interaction);
     });
 
+    // Imports all commands from ./commands/
     await importx(`${dirname(import.meta.url)}/commands/**/*.{js,ts}`);
   
+    // Login to bot
     if (!process.env.discord_token) {
         throw Error("Could not find token in your environment");
       }
       await client.login(process.env.discord_token);
-      console.log("bot >> discordgpt connected \n"); 
+      console.log("bot >> discordCGPT connected \n"); 
     }
   
   start();
