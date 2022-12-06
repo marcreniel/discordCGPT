c973bfb4d5d957e86fe1d3ca01da0b34b21a9f0b
imedra <marcbernardino2005@gmail.com>
2022-12-06 11:10:13 -0600

    added discordx, boilerplate code

---
+++ src/commands/choice.ts
@@ -0,0 +1,28 @@
+import type { CommandInteraction } from "discord.js";
+import { ApplicationCommandOptionType } from "discord.js";
+
+import {
+  Discord,
+  Slash,
+  SlashChoice,
+  SlashOption,
+} from "discordx";
+
+@Discord()
+export class Example {
+  @Slash({ description: "choice-test", name: "choice-test" })
+  min(
+    @SlashChoice({ name: "alex", value: "alex" })
+    @SlashChoice({ name: "mike", value: "mike" })
+    @SlashOption({
+      description: "input",
+      name: "input",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    input: string,
+    interaction: CommandInteraction
+  ): void {
+    interaction.reply(`${input}`);
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import "dotenv/config"
+import { Client } from "discordx";
+import { IntentsBitField } from "discord.js";
+import { dirname, importx } from "@discordx/importer";
+
+async function start() {
+    const client = new Client({
+        intents: [
+            IntentsBitField.Flags.Guilds,
+            IntentsBitField.Flags.GuildMessages,
+            IntentsBitField.Flags.GuildMembers,
+      ],
+        silent: false,
+    });
+  
+    client.once("ready", async () => {
+        await client.clearApplicationCommands();
+        await client.initApplicationCommands();
+    });
+  
+    client.on("interactionCreate", (interaction) => {
+        client.executeInteraction(interaction);
+    });
+
+    await importx(`${dirname(import.meta.url)}/commands/**/*.{js,ts}`);
+  
+    if (!process.env.token) {
+        throw Error("Could not find token in your environment");
+      }
+      await client.login(process.env.token);
+      console.log("bot >> discordgpt connected \n"); 
+    }
+  
+  start();
